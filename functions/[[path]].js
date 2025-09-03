9763e9408fa4ea7c24062956b574e75dc6b2d03a
cmliu <cmliu@users.noreply.github.com>
2025-09-03 20:36:13 +0800

    更新 .gitignore，新增 build-index.js 脚本以构建 index.html，修复 package.json 中的构建命令

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1332,7 +1332,7 @@ async function subHtml(request) {
 				<script src="https://cdn.jsdelivr.net/npm/@keeex/qrcodejs-kx@1.0.2/qrcode.min.js"></script>
 			</head>
 			<body>
-				<a href="${atob('aHR0cHM6Ly9naXRodWIuY29tL2NtbGl1L1dvcmtlclZsZXNzMnN1Yg==')}" target="_blank" class="github-corner" aria-label="View source on Github">
+				<a href="https://github.com/cmliu/EdgeOne-Pages-BestIP2SUB" target="_blank" class="github-corner" aria-label="View source on Github">
 					<svg viewBox="0 0 250 250" aria-hidden="true">
 						<path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
 						<path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path>
@@ -1346,7 +1346,7 @@ async function subHtml(request) {
 						</div>
 					<div class="input-group">
 						<label for="link">节点链接</label>
-						<input type="text" id="link" placeholder="${decodeURIComponent(atob('JUU4JUFGJUI3JUU4JUJFJTkzJUU1JTg1JUE1JTIwVk1lc3MlMjAlMkYlMjBWTEVTUyUyMCUyRiUyMFRyb2phbiUyMCVFOSU5MyVCRSVFNiU4RSVBNQ=='))}">
+						<input type="text" id="link" placeholder="请输入 VMess / VLESS / Trojan 链接">
 					</div>
 					
 					<button onclick="generateLink()">生成优选订阅</button>
